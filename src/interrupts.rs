9a1a6dab98d989e2bf87ffb51b73c6cc0b0cfca1
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-05-01 17:12:28 +0200

    Updated pc-keyboard to 0.7.0

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -76,6 +76,51 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     }
 }
 
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard};
+    use pc_keyboard::layouts::Us104Key;
+    use pc_keyboard::ScancodeSet1;
+    use crate::interrupts::spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> = Mutex::new(
+            Keyboard::new(
+                ScancodeSet1::new(),
+                Us104Key,
+                HandleControl::Ignore,
+            )
+        );
+    }
+    
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+    
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => {
+                    print!("{}", character);
+                },
+                DecodedKey::RawKey(key) => {
+                    //No usse for thiss now. may have ssome in the future
+                },
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
+
+
+
+/* 
+Older pc_keyboard = 0.5.0
 extern "x86-interrupt" fn keyboard_interrupt_handler( _stack_frame: InterruptStackFrame) {
     use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
     use spin::Mutex;
@@ -105,7 +150,7 @@ extern "x86-interrupt" fn keyboard_interrupt_handler( _stack_frame: InterruptSta
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
-}
+}*/
 
 #[test_case]
 fn test_breakpoint_exception() {
